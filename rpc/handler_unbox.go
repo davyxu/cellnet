03c5f3d63813e3ff3578056deec905341133e7a8
davyxu <sunicdavy@qq.com>
2017-10-07 16:57:32 +0800

    添加：注释

--- rpc/handler_unbox.go
+++ rpc/handler_unbox.go
@@ -19,6 +19,7 @@ func (self *UnboxHandler) Call(ev *cellnet.Event) {
 	// 服务器接收后, 发送时, 需要使用CallID
 	ev.TransmitTag = wrapper.CallID
 
+	// 方便在rpc消息接收后，使用ev.Send直接将反馈消息发给rpc请求方。发送过程是一个需要定制的过程
 	ev.ChainSend = self.feedbackChain
 
 }
--- rpc/reqhandler.go
+++ rpc/reqhandler.go
@@ -2,7 +2,7 @@ package rpc
 
 import "github.com/davyxu/cellnet"
 
-// 请求方接收消息
+// 请求方(rpc服务器）接收消息处理过程
 func buildRecvHandler(p cellnet.Peer, msgName string, tailHandler cellnet.EventHandler) (rpcID int64, err error) {
 
 	msgMeta := cellnet.MessageMetaByName(msgName)
@@ -13,11 +13,11 @@ func buildRecvHandler(p cellnet.Peer, msgName string, tailHandler cellnet.EventH
 	rpcID = p.AddChainRecv(cellnet.NewHandlerChain(
 		cellnet.NewMatchMsgIDHandler(MetaCall.ID),
 		cellnet.StaticDecodePacketHandler(),
-		NewUnboxHandler(ChainSend()),
+		NewUnboxHandler(ChainSend()), // 将rpc的发包过程保存在Handler中，每次解包时，能随着Event透传到Send中
 		cellnet.NewMatchMsgIDHandler(msgMeta.ID),
 		cellnet.StaticDecodePacketHandler(),
 		cellnet.NewQueuePostHandler(p.Queue(), tailHandler, cellnet.NewCallbackHandler(func(ev *cellnet.Event) {
-
+			// 处理完毕时，移除这个处理链
 			p.RemoveChainRecv(rpcID)
 
 		})),
