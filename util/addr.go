4e9bb9e625a003ff1d4dab432957e5c40980ac2b
xubo <sunicdavy@qq.com>
2018-08-27 16:38:18 +0800

    add 获得远程地址

--- util/addr.go
+++ util/addr.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"github.com/davyxu/cellnet"
 	"net"
 	"strconv"
 )
@@ -27,4 +28,12 @@ func SpliteAddress(addr string) (host string, port int, err error) {
 
 func JoinAddress(host string, port int) string {
 	return fmt.Sprintf("%s:%d", host, port)
+}
+
+func GetRemoteAddrss(ses cellnet.Session) (string, bool) {
+	if c, ok := ses.Raw().(net.Conn); ok {
+		return c.RemoteAddr().String(), true
+	}
+
+	return "", false
 }
