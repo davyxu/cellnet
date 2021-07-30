d038cd8dd8e77182f145cf593eb84b91ab32afbe
Davy xu <sunicdavy@qq.com>
2021-07-30 18:05:43 +0800

    try入口

--- peer/proctect.go
+++ peer/proctect.go
@@ -1,5 +1,7 @@
 package cellpeer
 
+import xos "github.com/davyxu/x/os"
+
 type Protect struct {
 	CapturePanic bool
 }
@@ -8,14 +10,7 @@ type Protect struct {
 func (self *Protect) ProctectCall(job func(), cleanup func(raw interface{})) {
 
 	if self.CapturePanic {
-
-		defer func() {
-			if raw := recover(); raw != nil {
-				cleanup(raw)
-			}
-		}()
-
-		job()
+		xos.Try(job, cleanup)
 	} else {
 		job()
 	}
