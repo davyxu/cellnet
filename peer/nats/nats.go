ffaac9ef5f33de6f51a5c6d651f65a8cd99ec2e0
davyxu <sunicdavy@qq.com>
2021-10-04 21:35:12 +0800

    加强安全性

--- peer/nats/nats.go
+++ peer/nats/nats.go
@@ -1,9 +1,14 @@
 package nats
 
 import (
+	"errors"
 	natstransport "github.com/davyxu/cellnet/transport/nats"
 	"github.com/nats-io/nats.go"
 	"time"
+)
+
+var (
+	ErrMsgqNotReady = errors.New("msgq not ready")
 )
 
 type MsgQueue struct {
@@ -19,10 +24,17 @@ func (self *MsgQueue) Publish(topic string, msg interface{}) error {
 		return err
 	}
 
+	if self.nc == nil {
+		return ErrMsgqNotReady
+	}
+
 	return self.nc.Publish(topic, payload)
 }
 
 func (self *MsgQueue) Subscribe(topic string, callback func(msg interface{}, err error) interface{}) error {
+	if self.nc == nil {
+		return ErrMsgqNotReady
+	}
 	_, err := self.nc.Subscribe(topic, func(raw *nats.Msg) {
 		msg, err := self.OnRecv(raw.Data)
 		reply := callback(msg, err)
@@ -40,6 +52,10 @@ func (self *MsgQueue) Request(topic string, msg interface{}, timeout time.Durati
 	if err != nil {
 		retErr = err
 		return
+	}
+
+	if self.nc == nil {
+		return nil, ErrMsgqNotReady
 	}
 
 	reply, err := self.nc.Request(topic, payload, timeout)
